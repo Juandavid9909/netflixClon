59e7ed41966933f4abf9e04c191d0968aa3ca30a
Juan David <juandavid918@gmail.com>
2023-05-23 15:27:30 -0500

    Cambiado title en watch

--- pages/watch/[movieId].tsx
+++ pages/watch/[movieId].tsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 
 import useMovie from "@/hooks/useMovie";
 import { AiOutlineArrowLeft } from "react-icons/ai";
+import Head from "next/head";
 
 const Watch = () => {
     const router = useRouter();
@@ -11,24 +12,31 @@ const Watch = () => {
     const { data } = useMovie(movieId as string);
 
     return (
-        <div className="h-screen w-screen bg-black">
-            <nav className="fixed w-full p-4 z-10 flex flex-row items-center gap-8 bg-black bg-opacity-70">
-                <AiOutlineArrowLeft onClick={ () => router.push("/") } className="text-white cursor-pointer" size={ 40 } />
+        <>
+            <Head>
+                <title>juanvarela-netflix - { data?.title }</title>
+                <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+            </Head>
 
-                <p className="text-white text-1xl md:text-3xl font-bold">
-                    <span className="font-light">Watching:</span>
+            <div className="h-screen w-screen bg-black">
+                <nav className="fixed w-full p-4 z-10 flex flex-row items-center gap-8 bg-black bg-opacity-70">
+                    <AiOutlineArrowLeft onClick={ () => router.push("/") } className="text-white cursor-pointer" size={ 40 } />
 
-                    { data?.title }
-                </p>
-            </nav>
+                    <p className="text-white text-1xl md:text-3xl font-bold">
+                        <span className="font-light">Watching:</span>
 
-            <video
-                autoPlay
-                controls
-                className="h-full w-full"
-                src={ data?.videoUrl }
-            ></video>
-        </div>
+                        { data?.title }
+                    </p>
+                </nav>
+
+                <video
+                    autoPlay
+                    controls
+                    className="h-full w-full"
+                    src={ data?.videoUrl }
+                ></video>
+            </div>
+        </>
     );
 }
 
